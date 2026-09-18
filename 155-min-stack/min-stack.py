3f45cd69f968985f3f3e939ca5db40e1c654252d
Ashlyn Lee <68078477+4shlyn@users.noreply.github.com>
2026-09-18 15:38:38 -0400

    Time: 116 ms (22.14%) | Memory: 31.8 MB (56.39%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,49 +1,39 @@
-class ListNode:
-    def __init__(self, val:int):
-         self.val = val
-         self.next = None
-         self.min = 9223372036854775807
-
 class MinStack:
+
     def __init__(self):
-        self.head = ListNode(0)
-        self.len = 0
-        self.curMin = 9223372036854775807
+        self.stack=[]
+        self.mins = []
+        self.min=1000
 
-
-    def push(self, val: int) -> None:
-        n = ListNode(val)
-        n.next = self.head.next
-        self.head.next = n
-        if self.curMin > val or self.curMin == None:
-            self.curMin = val
-        n.min = min(self.curMin, val)
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+        if value < self.min:
+            self.min = value
+        if len(self.stack) >=2:
+            if value < self.mins[len(self.stack)-2]:
+                self.mins.append(value)
+            else:
+                self.mins.append(self.mins[len(self.stack)-2])
+        else:
+            self.mins.append(value)
 
     def pop(self) -> None:
-        # set the head's next to the head's next next
-        if self.head.next:
-            if self.head.next.min == self.curMin:
-                if self.head.next.next:
-                    self.curMin = self.head.next.next.min
-                else:
-                    self.curMin = 9223372036854775807
-            if self.head.next.next:
-                self.head.next = self.head.next.next
-            else:
-                self.head.next = None
+        self.stack.pop()
+        self.mins.pop()
+        
 
     def top(self) -> int:
-        if self.head.next:
-            return self.head.next.val
+        return self.stack[len(self.stack)-1]
+        
 
     def getMin(self) -> int:
-        return self.head.next.min
+        return self.mins[len(self.mins)-1]
         
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(val)
+# obj.push(value)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
